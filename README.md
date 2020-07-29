eb52df3f8149dd8de1e5e177b3221fec196adcb8
elmiguel0 <elmiguelo@live.com.mx>
2020-07-29 13:09:57 -0500

    Start Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Spring RESTFul Web Service
+tomando la oportunidad de mojarme los pies en esta tecnologia.
+Usando la API reqres.in
---
+++ src/main/java/org/miguelramos/api/domain/Ad.java
@@ -0,0 +1,45 @@
+package org.miguelramos.api.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ad {
+
+    private String company;
+    private String url;
+    private String text;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
